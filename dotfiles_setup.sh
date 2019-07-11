75fd56224040b56f0fc9d4a8008ea441c2848cbd
Paul Straberger <48472437+pastra98@users.noreply.github.com>
2019-07-11 22:03:10 +0200

    finished setup file for now

--- dotfiles_setup.sh
+++ dotfiles_setup.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
-# this makes forces the file to only open in bash
+# this makes forces the file to only open in bash.
+# this file will create a link in the home directory to every dotfile listed in the
+# dotfiles variable.
 
+# all used dotfiles in one variable.
+dotfiles=(".vimrc")
+
+# variable of the path to the directory holding my dotfiles.
+dir="${HOME}/dotfiles"
+
+# loops through listed dotfiles from var, and links it to the home directory where it belongs.
+for dotfile in "${dotfiles[@]}";do
+ ln -sf "${HOME}/${dotfile}" "${dir}"
+done
